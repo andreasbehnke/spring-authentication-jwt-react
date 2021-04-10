43fa4c6f624b88655d229fdfbc9e853bbec29ee6
andreasbehnke <ab@andreasbehnke.com>
2021-04-10 19:19:19 +0200

    initialized spring boot backend

---
+++ backend/src/main/java/org/buildingblock/springauthjwt/SpringAuthJwtApplication.java
@@ -0,0 +1,13 @@
+package org.buildingblock.springauthjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAuthJwtApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/buildingblock/springauthjwt/SpringAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.buildingblock.springauthjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
