91733f16e5dc13b0cc8879d6fe66f585ea17f35b
andreasbehnke <ab@andreasbehnke.com>
2021-04-13 21:00:57 +0200

    added styled react login form

--- webclient/src/App.tsx
+++ webclient/src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import {Box, Button, Grid, Paper, TextField, Typography} from "@material-ui/core";
 
-function App() {
+export default function App() {
   return (
-      <div>Hello World!</div>
+      <Grid container justify={"center"}>
+          <Box p={2} mt={15} style={{maxWidth: "400px"}}>
+              <Paper elevation={4}>
+                  <Box p={2}>
+                      <Grid container spacing={2}>
+                          <Grid item xs={12}><Typography variant={"h6"}>Login</Typography></Grid>
+                          <Grid item xs={12}><TextField fullWidth label={"E-Mail"} /></Grid>
+                          <Grid item xs={12}><TextField fullWidth label={"Password"} /></Grid>
+                          <Grid item container xs={12} justify={"flex-end"}>
+                              <Button variant="contained" color="primary">Login</Button>
+                          </Grid>
+                      </Grid>
+                  </Box>
+              </Paper>
+          </Box>
+      </Grid>
   );
 }
-
-export default App;
