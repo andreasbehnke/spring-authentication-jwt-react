e05e39ada490dda4d5325073ea1d6aa6aead9529
andreasbehnke <ab@andreasbehnke.com>
2021-04-13 21:05:27 +0200

    added made password input hidden, set autocomplete attribute

--- webclient/src/App.tsx
+++ webclient/src/App.tsx
@@ -9,8 +9,8 @@ export default function App() {
                   <Box p={2}>
                       <Grid container spacing={2}>
                           <Grid item xs={12}><Typography variant={"h6"}>Login</Typography></Grid>
-                          <Grid item xs={12}><TextField fullWidth label={"E-Mail"} /></Grid>
-                          <Grid item xs={12}><TextField fullWidth label={"Password"} /></Grid>
+                          <Grid item xs={12}><TextField fullWidth label={"E-Mail"} autoComplete={"email"}/></Grid>
+                          <Grid item xs={12}><TextField fullWidth label={"Password"} type="password" autoComplete={"current-password"} /></Grid>
                           <Grid item container xs={12} justify={"flex-end"}>
                               <Button variant="contained" color="primary">Login</Button>
                           </Grid>
