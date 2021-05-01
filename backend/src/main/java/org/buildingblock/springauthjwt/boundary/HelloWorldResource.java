1c2e9b8aa1b15016cd04cc29c7734dc79eda5730
andreasbehnke <ab@andreasbehnke.com>
2021-05-01 17:44:13 +0200

    REST Resources reside in package boundary

--- backend/src/main/java/org/buildingblock/springauthjwt/boundary/HelloWorldResource.java
+++ backend/src/main/java/org/buildingblock/springauthjwt/boundary/HelloWorldResource.java
@@ -1,4 +1,4 @@
-package org.buildingblock.springauthjwt;
+package org.buildingblock.springauthjwt.boundary;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
