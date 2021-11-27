38bba2261ce8e67bfbe9f42e6c91b8b213b7bf65
andreasbehnke <ab@andreasbehnke.com>
2021-11-27 11:24:32 +0100

    introduced form validation to ForgotPasswordForm

--- webclient/src/ForgotPasswordForm.tsx
+++ webclient/src/ForgotPasswordForm.tsx
@@ -1,56 +1,62 @@
-import {Button, Grid, TextField, Typography} from "@material-ui/core";
-import React, {useState} from "react";
+import {Button, Grid, Typography} from "@material-ui/core";
+import React from "react";
 import axios from "axios";
 import {useSnackbar} from "notistack";
 import {useHistory} from "react-router-dom";
 import UserForgotPasswordRequest from "./dto/UserForgotPasswordRequest";
+import {FormProvider, useForm} from "react-hook-form";
+import {ControlledTextField} from "./common/ControlledTextField";
 
 export default function ForgotPasswordForm() {
 
-    const [submitting, setSubmitting] = useState<boolean>(false);
+    const methods = useForm({mode: "onBlur"});
+    const {handleSubmit, formState: {isSubmitting, isValid}} = methods;
 
-    const [username, setUsername] = useState<string>("", );
-
-    const { enqueueSnackbar } = useSnackbar();
+    const {enqueueSnackbar} = useSnackbar();
 
     const history = useHistory();
 
-    const submitForgotPasswordRequest = async() => {
-        if (submitting) {
-            return;
-        }
-        setSubmitting(true);
-        await axios.post<UserForgotPasswordRequest>("/public/forgotPassword", { username } )
+    const onSubmit = async (data: UserForgotPasswordRequest) => {
+        return await axios.post<UserForgotPasswordRequest>("/public/forgotPassword", data)
             .then(() => {
                 history.push("/login");
-                enqueueSnackbar("Password reset link has been send", { variant: "info" });
+                enqueueSnackbar("Password reset link has been send", {variant: "info"});
             })
             .catch(() => {
-                enqueueSnackbar("Sending password reset link failed", { variant: "error" });
-            })
-            .finally(() => setSubmitting(false));
+                enqueueSnackbar("Sending password reset link failed", {variant: "error"});
+            });
     };
 
     return (
-        <Grid container spacing={2}>
-            <Grid item xs={12}><Typography variant={"h6"}>Forgot Password</Typography></Grid>
-            <Grid item xs={12}>
-                <TextField
-                    fullWidth
-                    label={"E-Mail"}
-                    autoComplete={"email"}
-                    value={username}
-                    onChange={event => { setUsername(event.target.value) }}
-                />
-            </Grid>
-            <Grid item container xs={12} justify={"flex-end"}>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={submitForgotPasswordRequest}
-                    disabled={submitting}
-                >Send Password Reset E-Mail</Button>
-            </Grid>
-        </Grid>
+        <FormProvider {...methods}>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}><Typography variant={"h6"}>Forgot Password</Typography></Grid>
+                    <Grid item xs={12}>
+                        <ControlledTextField
+                            name={"username"}
+                            fullWidth
+                            label={"E-Mail"}
+                            autoComplete={"email"}
+                            rules={{
+                                pattern: {
+                                    value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+                                    message: "enter a valid e-mail address"
+                                },
+                                required: {value: true, message: "enter an e-mail address"}
+                            }}
+                        />
+                    </Grid>
+                    <Grid item container xs={12} justify={"flex-end"}>
+                        <Button
+                            type={"submit"}
+                            variant="contained"
+                            color="primary"
+                            disabled={isSubmitting || !isValid}
+                        >Send Password Reset E-Mail</Button>
+                    </Grid>
+                </Grid>
+            </form>
+        </FormProvider>
     );
 }
